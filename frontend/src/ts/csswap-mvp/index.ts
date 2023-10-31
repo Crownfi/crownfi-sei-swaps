37f6dc4b129f8d767ccb1bd6113b54b40a678a69
ARitz Cracker <aritz@aritzcracker.ca>
2023-10-31 22:26:44 +0000

    wip: frontend * add assets, develop styles more

--- frontend/src/ts/csswap-mvp/index.ts
+++ frontend/src/ts/csswap-mvp/index.ts
@@ -3,16 +3,21 @@ import { setLoading } from "./loading";
 import "./wallet_chooser";
 import { FarmPoolComponentElement } from "./components/farm";
 import { SwapComponentElement } from "./components/swap";
-import { errorDialogIfRejected, q } from "./util";
+import { errorDialogIfRejected, q, qa } from "./util";
 import { MaybeSelectedProvider, getSelectedChain, setPreferredSeiProvider } from "./wallet-env";
 import { KNOWN_SEI_PROVIDER_INFO } from "@crownfi/sei-js-core";
-
+function removeHighlightsFromLinks() {
+	(qa(".header .header-links a") as NodeListOf<HTMLAnchorElement>).forEach(v => {
+		v.classList.remove("highlighted");
+	});
+}
 export async function main() {
 	const mainSwapButton = q("#swap-link") as HTMLAnchorElement;
 	const mainFarmButton = q("#farm-link") as HTMLAnchorElement;
 	const mainContent = q("#main-content") as HTMLElement;
 	mainSwapButton.onclick = (ev) => {
 		ev.preventDefault();
+		removeHighlightsFromLinks();
 		errorDialogIfRejected(async () => {
 			mainContent.innerHTML = "";
 			const swapComponent = new SwapComponentElement();
@@ -20,18 +25,19 @@ export async function main() {
 			mainContent.appendChild(
 				swapComponent
 			);
-		})
+		});
+		mainSwapButton.classList.add("highlighted");
 	};
 	mainFarmButton.onclick = (ev) => {
-		//
 		ev.preventDefault();
+		removeHighlightsFromLinks();
 		errorDialogIfRejected(async () => {
 			mainContent.innerHTML = "";
 			mainContent.appendChild(
 				new FarmPoolComponentElement()
 			);
 		});
-		
+		mainFarmButton.classList.add("highlighted");
 	}
 	let storedPreferredProvider = localStorage.getItem("preferred_sei_provider");
 	if (
