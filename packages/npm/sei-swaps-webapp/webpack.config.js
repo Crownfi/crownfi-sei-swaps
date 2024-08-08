5896d20b1ea1a4451bcada2b1fdc1ff224bba462
Vitor Hugo <vhcsilvaa@gmail.com>
2024-08-08 20:43:26 -0300

    create watchRun hook to improve productivity

--- packages/npm/sei-swaps-webapp/webpack.config.js
+++ packages/npm/sei-swaps-webapp/webpack.config.js
@@ -1,14 +1,21 @@
+import { execSync } from "child_process";
+import { glob } from "glob";
 import { resolve as pathResolve } from "path";
 import { fileURLToPath } from "url";
 import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+
 const __dirname = import.meta.dirname;
 
 export default (env, argv) => {
 	const config = {
 		entry: {
-			main: ["./src/entrypoint.ts", "./styles/main.css"],
+			main: [
+				"./src/entrypoint.ts", 
+				"./styles/main.css",
+				...glob.sync("./src/**/template.html", { dotRelative: true })
+			],
 		},
 		module: {
 			rules: [
@@ -20,6 +27,10 @@ export default (env, argv) => {
 				{
 					test: /\.css$/,
 					use: [MiniCssExtractPlugin.loader, "css-loader"],
+				},
+				{
+					test: /template\.html$/i,
+					use: "html-loader"
 				},
 			],
 		},
@@ -43,6 +54,16 @@ export default (env, argv) => {
 			new webpack.ProvidePlugin({
 				Buffer: ["buffer-lite", "Buffer"],
 			}),
+			{
+				apply: (compiler) => {
+					compiler.hooks.watchRun.tap("WatchRun", () => {
+						if (!compiler.modifiedFiles) return;
+
+						if (Array.from(compiler.modifiedFiles).some(f => f.includes("template.html")))
+							execSync("npm run codegen");
+					});
+				}
+			}
 		],
 		optimization: {
 			minimizer: [`...`, new CssMinimizerPlugin()],
