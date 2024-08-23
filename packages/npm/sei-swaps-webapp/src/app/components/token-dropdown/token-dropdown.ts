ab844e0400927f350d97771cb62f67ac0c3cde86
Vitor Hugo <vhcsilvaa@gmail.com>
2024-08-23 14:36:44 -0300

    introduce reusable token dropdown component

---
+++ packages/npm/sei-swaps-webapp/src/app/components/token-dropdown/token-dropdown.ts
@@ -0,0 +1,59 @@
+import { DropdownMenuItemElement } from "dropdown-menu-element";
+import { TokenDropdownComponentAutogen } from "./_autogen/token-dropdown.js";
+import { TokenDisplayElement } from "@crownfi/sei-webui-utils";
+
+type TokenDropdownSelectedEventDetails = {
+  id: string;
+  denom: string;
+};
+
+export type TokenDropdownSelectedEvent = CustomEvent<TokenDropdownSelectedEventDetails>;
+
+declare global {
+	interface GlobalEventHandlersEventMap {
+		"tokenDropdownSelected": TokenDropdownSelectedEvent
+	}
+}
+
+export class TokenDropdrownComponent extends TokenDropdownComponentAutogen {  
+  constructor() {
+    super();
+  }
+
+  onSelectedChanged(oldValue: string, newValue: string) {
+    this.refs.selectedToken.setAttribute("denom", newValue);
+  }
+
+  onOptionsChanged(oldValue: string, newValue: string): void {
+    const tokens = newValue.split(",");
+    this.refs.tokensDropdown.innerHTML = "";
+
+    for (const token of tokens) {
+      const option = new DropdownMenuItemElement();
+      const tokenDisplay = new TokenDisplayElement();
+
+      tokenDisplay.setAttribute("denom", token);
+      option.value = token;
+      option.appendChild(tokenDisplay);
+      this.refs.tokensDropdown.appendChild(option);
+    }
+  }
+
+  connectedCallback() {
+    const id = this.getAttribute("id");
+    this.classList.add("fantasy", "primary");
+    this.refs.tokensDropdown.setAttribute("linked-elements", `#${id}`);
+    this.addEventListener("dropdownSelect", ev => {
+      ev.stopPropagation();
+      const denom = ev.detail.selectedValue;
+      if (!id || !denom) return;
+      const detail = {
+        id,
+        denom,
+      } satisfies TokenDropdownSelectedEventDetails;
+      this.dispatchEvent(new CustomEvent("tokenDropdownSelected", { detail, bubbles: true }));
+    });
+  }
+}
+
+TokenDropdrownComponent.registerElement();
