d2b67b3838ee66083072757eef8f9664ace87421
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2024-08-26 17:36:13 -0400

    Implement volume queries for SwapMarket (+ other changes) * Add new pair search error types * un-hide SwapMarketPair's canonAssets properties * have SwapMarketPair's canonAssets and assets properties be read-only * add isPair function to SwapMarketPair * clarify return type for assetTradeVolume functions on SwapMarketPair * minor docs changes * Add normalized volume queries for SwapMarket class

--- packages/npm/sei-swaps-sdk/src/error.ts
+++ packages/npm/sei-swaps-sdk/src/error.ts
@@ -1,3 +1,5 @@
+import { UnifiedDenom, UnifiedDenomPair } from "./types.js";
+
 export class InvalidDenomError extends Error {
 	name!: "InvalidDenomError";
 	invalidDenom: string;
@@ -15,3 +17,23 @@ export class InvalidDenomError extends Error {
 	}
 }
 InvalidDenomError.prototype.name = "InvalidDenomError";
+
+export class MarketPairNotFoundError extends Error {
+	name!: "MarketPairNotFoundError";
+	pair: UnifiedDenomPair;
+	constructor(pair: UnifiedDenomPair) {
+		super("The trading pair " + pair.join("<>") + " could not be found")
+		this.pair = pair;
+	}
+}
+MarketPairNotFoundError.prototype.name = "MarketPairNotFoundError";
+
+export class UnsatisfiableSwapRouteError extends Error {
+	name!: "UnsatisfiableSwapRouteError";
+	constructor(
+		public from: UnifiedDenom,
+		public to: UnifiedDenom
+	) {
+		super("The swap market has no means to trade between " + from + " and " + to);
+	}
+}
