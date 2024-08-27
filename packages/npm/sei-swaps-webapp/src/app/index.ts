ebc8a70a5f16f333efd5a3fd0abf780b9f57e1dc
Vitor Hugo <vhcsilvaa@gmail.com>
2024-08-27 17:15:53 -0300

    remove unused code

--- packages/npm/sei-swaps-webapp/src/app/index.ts
+++ packages/npm/sei-swaps-webapp/src/app/index.ts
@@ -3,25 +3,13 @@ import "dropdown-menu-element";
 
 import "./components/exports.js";
 
-import { alert, errorDialogIfRejected } from "./dialogs/index.js";
-
-//import { FarmPoolComponentElement } from "./components/farm";
-import { SwapComponentElement } from "./components/swap/index.js";
-import { q, qa } from "./util.js";
-import { KNOWN_SEI_PROVIDER_INFO } from "@crownfi/sei-js-core";
-import { SeiChainId, seiUtilEventEmitter, setDefaultNetwork, setNetworkConfig } from "@crownfi/sei-utils";
-import { FarmPoolComponentElement } from "./components/farm/index.js";
+import { seiUtilEventEmitter, setDefaultNetwork } from "@crownfi/sei-utils";
 import { SwapService } from "../services/swap-service.js";
 
 import { env } from "../env/index.js";
 import { AppContainer } from "./components/exports.js";
 import { WebClientEnv } from "@crownfi/sei-webui-utils";
-
-function removeHighlightsFromLinks() {
-	(qa(".header .header-links a") as NodeListOf<HTMLAnchorElement>).forEach((v) => {
-		v.classList.remove("highlighted");
-	});
-}
+import { q } from "@aritz-cracker/browser-utils";
 
 export let swapService: SwapService;
 
@@ -37,42 +25,6 @@ export async function main() {
 	const mainContent = q("#main-content") as HTMLElement;
 	mainContent.innerHTML = "";
 	mainContent.appendChild(new AppContainer());
-
-	// const mainSwapButton = q("#swap-link") as HTMLAnchorElement;
-	// const mainFarmButton = q("#farm-link") as HTMLAnchorElement;
-	// const mainContent = q("#main-content") as HTMLElement;
-	// mainSwapButton.onclick = (ev) => {
-	// 	ev.preventDefault();
-	// 	removeHighlightsFromLinks();
-	// 	errorDialogIfRejected(async () => {
-	// 		mainContent.innerHTML = "";
-	// 		const swapComponent = new SwapComponentElement();
-	// 		swapComponent.className = "border-img-scroll-25";
-	// 		mainContent.appendChild(swapComponent);
-	// 	});
-	// 	mainSwapButton.classList.add("highlighted");
-	// };
-	// mainFarmButton.onclick = (ev) => {
-	// 	ev.preventDefault();
-	// 	removeHighlightsFromLinks();
-	// 	errorDialogIfRejected(async () => {
-	// 		mainContent.innerHTML = "";
-	// 		mainContent.appendChild(new FarmPoolComponentElement());
-	// 	});
-	// 	mainFarmButton.classList.add("highlighted");
-	// };
-
-	// let storedNetworkPref = localStorage.getItem("preferred_sei_network");
-
-	// if (storedNetworkPref == null) {
-	// 	storedNetworkPref =
-	// 		document.location.host.startsWith("127.0.0.1") || document.location.host.startsWith("localhost")
-	// 			? "sei-chain"
-	// 			: "atlantic-2";
-	// }
-
-	// mainSwapButton.click();
-	// setLoading(false);
 }
 
 seiUtilEventEmitter.on("defaultProviderChanged", ({ provider, chainId }) => {
