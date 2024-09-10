f26d40b3a572cff53bcf27ca536bf1962168476b
Vitor Hugo <vhcsilvaa@gmail.com>
2024-09-10 19:19:24 -0300

    refactor exports

---
+++ packages/npm/sei-swaps-webapp/src/app/components/farm/exports.ts
@@ -0,0 +1,5 @@
+export * from "./farm.js";
+export * from "./pool-item/pool-item.js";
+export * from "./sort-by/sort-by.js";
+export * from "./filters/filters.js";
+export * from "./pool-dialog/pool-dialog.js";
---
+++ packages/npm/sei-swaps-webapp/src/app/components/swap/exports.ts
@@ -0,0 +1,4 @@
+export * from "./swap.js";
+export * from "./from-token/from-token.js";
+export * from "./to-token/to-token.js";
+export * from "./dialogs/confirm-swap/confirm-swap.js";
